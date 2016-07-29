478443aeaf2776ea20cc3f5eff04ba9813a3e05e
Gil Mendes <gil00mendes@gmail.com>
2016-07-29 17:56:24 +0100

    first commit

---
+++ config/auth.js
@@ -0,0 +1,46 @@
+'use strict'
+
+exports.default = {
+  auth: api => {
+    return {
+      // ---------------------------------------------------------------------
+      // Secret token to generate the JWT token
+      // ---
+      // This must be changed on the application configs.
+      // ---------------------------------------------------------------------
+      secret: 'thisMustBeChangedOnAppConfig',
+
+      // ---------------------------------------------------------------------
+      // Time until the token expires
+      // ---
+      // Expressed in seconds or a string describing a time span rauchg/ms
+      // ---------------------------------------------------------------------
+      expiresIn: '1 day',
+
+      errors: {
+        // don't expand this
+        '_toExpand': false,
+
+        // ---------------------------------------------------------------------
+        // Token not provided
+        // ---------------------------------------------------------------------
+        tokenNotProvided: () => 'Token not provided',
+
+        // ---------------------------------------------------------------------
+        // Malformed token
+        // ---------------------------------------------------------------------
+        malformedToken: () => 'The token is invalid',
+
+        // ---------------------------------------------------------------------
+        // Expired token
+        // ---------------------------------------------------------------------
+        expiredToken: () => 'The token was expired',
+
+        // ---------------------------------------------------------------------
+        // Wrong username or password
+        // ---------------------------------------------------------------------
+        invalidCredentials: () => 'Invalid credentials'
+      }
+    }
+  }
+}
---
+++ middleware/needAuth.js
@@ -0,0 +1,38 @@
+let jwt = require('jsonwebtoken')
+
+exports.needAuth = {
+  name: 'auth.needAuth',
+  description: 'The user needs authentication to access the action',
+
+  preProcessor: (action, next) => {
+    // get the API reference
+    let api = action.api
+
+    // check action params for token
+    let token = action.params.token
+
+    // if the token was not found on the action params and
+    // the connection is HTTP check the headers
+    if (token === undefined && action.connection.type === 'web') {
+      token = action.connection.rawConnection.req.headers[ 'x-access-token' ]
+    }
+
+    // decode token
+    if (token) {
+      // verify secret and check exp
+      jwt.verify(token, api.config.auth.secret, (error, decoded) => {
+        if (error) { return next(new Error(api.config.auth.errors.malformedToken())) }
+
+        // if everything is good, save was an action property to be used later
+        action.authDecodedToken = decoded
+
+        // execute the callback function
+        return next()
+      })
+
+      return
+    }
+
+    return next(new Error(api.config.auth.errors.tokenNotProvided()))
+  }
+}
