a2ed81b5146f322e59b3d92d21f3171379601ed9
Gil Mendes <gil00mendes@gmail.com>
2016-09-28 15:45:26 +0100

    remove the password fields from the output messages

--- models/user.js
+++ models/user.js
@@ -1,8 +1,24 @@
 'use strict'
 
-exports.default = {
-  email: { type : String , unique : true, required : true },
-  password: { type: String, required: true },
-  resetToken: String,
-  resetTokenExpire: Date
+exports.default = (api, mongoose) => {
+  // get Schema type
+  const Schema = mongoose.Schema
+
+  // create a new schema
+  const userSchema = new Schema({
+    email: { type : String , unique : true, required : true },
+    password: { type: String, required: true},
+    resetToken: String,
+    resetTokenExpire: Date
+  })
+
+  // define a custom `toJSON` method to remove the password from the output
+  userSchema.method('toJSON', function () {
+    const user = this.toObject()
+    delete user.password
+    return user
+  })
+
+  // return the new schema
+  return userSchema
 }
